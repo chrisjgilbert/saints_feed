770774512fa41f88fe7b488d3b0a65ff09be277a
Chris Gilbert <chris.gilbert@multiverse.io>
2021-11-19 10:43:30 +0000

    Force SSL

--- config/prod.exs
+++ config/prod.exs
@@ -45,7 +45,7 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-#     config :saints_feed, SaintsFeedWeb.Endpoint,
-#       force_ssl: [hsts: true]
+config :my_app, SaintsFeed.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
+
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
