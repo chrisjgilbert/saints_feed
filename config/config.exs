b66bb71ed90c9e143b7a7591e87651eaad032fd7
Chris Gilbert <chris.gilbert@multiverse.io>
2021-11-21 11:39:10 +0000

    test fix of config

--- config/config.exs
+++ config/config.exs
@@ -65,4 +65,4 @@ config :saints_feed, SaintsFeed.Scheduler,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
+import_config "#{Mix.env()}.exs"
